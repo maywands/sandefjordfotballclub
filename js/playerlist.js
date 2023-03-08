0c88e70925704d8839b497eb0598f52fc27b2e6c
maywands <mayo_jan@outlook.com>
2023-03-08 11:17:29 +0100

    laget til nytt arrays

---
+++ js/playerlist.js
@@ -0,0 +1,90 @@
+const myobjArray = [ 
+    { 
+      name: "Hugo Oliver Keto",
+      Posisjon: "Keeper",
+      jerseyNumber: 1,
+  
+    },
+    {
+      name: "Andreas brede Albertsen",
+      Posisjon: "Keeper",
+      jerseyNumber: 54,
+  
+    },
+    {
+      name: "Fredrik Mani Pålerud",
+      Posisjon: "Forsvarspiller",
+      jerseyNumber: 2,
+  
+    },
+  
+    {
+      name: "Quint Arie Jansen",
+      Posisjon: "Forsvarspiller",
+      jerseyNumber: 3,
+  
+    },
+    {
+      name: "lan Smeulers",
+      Posisjon: "Forsvarspiller",
+      jerseyNumber: 4,
+  
+    },
+   {
+    name: "Lara Markmanrud",
+    Posisjon: "Forsvarspiller",
+    jerseyNumber: 13,
+   },
+  {
+    name: "Jesper Taaje",
+    Posisjon: "Forsvarspiller",
+    jerseyNumber: 15,
+  }, 
+  ]; 
+
+
+const myobjArray = [ 
+    { 
+      name: "Hugo Oliver Keto",
+      Posisjon: "Keeper",
+      jerseyNumber: 1,
+  
+    },
+    {
+      name: "Andreas brede Albertsen",
+      Posisjon: "Keeper",
+      jerseyNumber: 54,
+  
+    },
+    {
+      name: "Fredrik Mani Pålerud",
+      Posisjon: "Forsvarspiller",
+      jerseyNumber: 2,
+  
+    },
+  
+    {
+      name: "Quint Arie Jansen",
+      Posisjon: "Forsvarspiller",
+      jerseyNumber: 3,
+  
+    },
+    {
+      name: "lan Smeulers",
+      Posisjon: "Forsvarspiller",
+      jerseyNumber: 4,
+  
+    },
+   {
+    name: "Lara Markmanrud",
+    Posisjon: "Forsvarspiller",
+    jerseyNumber: 13,
+   },
+  {
+    name: "Jesper Taaje",
+    Posisjon: "Forsvarspiller",
+    jerseyNumber: 15,
+  }, 
+  ]; 
+
+  
